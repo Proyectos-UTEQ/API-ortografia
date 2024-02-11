109b9a9327669f622de6632a2d49618319d889dc
RobertoSuarez <electrosonix12@gmail.com>
2024-02-11 13:16:01 -0500

    Elimine varias variables de entorno

--- internal/db/init.go
+++ internal/db/init.go
@@ -1,8 +1,6 @@
 package db
 
 import (
-	"fmt"
-
 	"github.com/spf13/viper"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,16 +12,16 @@ var DB *gorm.DB
 func ConnectDB(config *viper.Viper) *gorm.DB {
 
 	// Get config
-	host := config.GetString("DB_HOST")
-	port := config.GetString("DB_PORT")
-	user := config.GetString("DB_USER")
-	password := config.GetString("DB_PASSWORD")
-	name := config.GetString("DB_NAME")
-	sslmode := config.GetString("DB_SSLMODE")
-
-	// Build DSN
-	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=%v TimeZone=America/Guayaquil", host, user, password, name, port, sslmode)
-
+	//host := config.GetString("DB_HOST")
+	//port := config.GetString("DB_PORT")
+	//user := config.GetString("DB_USER")
+	//password := config.GetString("DB_PASSWORD")
+	//name := config.GetString("DB_NAME")
+	//sslmode := config.GetString("DB_SSLMODE")
+	//
+	//// Build DSN
+	//dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=%v TimeZone=America/Guayaquil", host, user, password, name, port, sslmode)
+	dsn := config.GetString("DB_DSN")
 	configDB := &gorm.Config{}
 	if config.GetBool("DB_DEBUG") {
 		configDB = &gorm.Config{
